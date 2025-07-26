f53a8f96f522ac111ee996dd81f8b4d3f93c4910
Mashhod Bin Shahid <mashhodbinshahid11@gmail.com>
2025-07-27 01:45:46 +0500

    done

--- server.mjs
+++ server.mjs
@@ -15,7 +15,7 @@ import { createServer } from 'http';
 
 const app = express();
 
-const PORT = process.env.PORT || 5005;
+const PORT = process.env.PORT || 3000;
 
 
 const server = createServer(app);
